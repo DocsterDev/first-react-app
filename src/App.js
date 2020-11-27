e04974aab0c0e00ab7f152086d84ed9fc01c9bc9
Jeff <jeff@artfx.io>
2020-11-27 09:56:00 -0500

    Update to tutorial code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+   state = {
+       persons: [
+           { name: 'Max', age: '67' },
+           { name: 'Jeff', age: '37' },
+           { name: 'Stephanie', age: '34' }
+       ]
+   }
+
+   switchNameHandler = (newName) => {
+       this.setState({
+           persons: [
+               { name: newName, age: '37' },
+               { name: 'Jeff', age: '39' },
+               { name: 'Stephanie', age: '34' }
+           ]
+       })
+   }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Testing</h1>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
+                    click={() => this.switchNameHandler('Sam')} />
+                <Person
+                    name="Max"
+                    age="33"
+                    click={this.switchNameHandler.bind(this, 'Jennifer')}>Stuff In Between</Person>
+                <Person
+                    name="Mike"
+                    age="36" />
+            </div>
+        );
+    }
 }
 
 export default App;
